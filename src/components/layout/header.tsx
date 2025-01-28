527ce7d68e241ff0bf288dc074f431885c48113c
choi-jiae <96973901+choi-jiae@users.noreply.github.com>
2025-01-28 22:53:17 +0900

    header

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,15 +1,28 @@
 import React from 'react';
+import { InputBase, IconButton, Button } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+import '../../css/header.css';
 
 const Header = () => {
   return (
     <header className="header">
-        <div>
+        <div className='logo'>
           PairPick
         </div>
         <nav>
-          <a href="/">홈</a>
-          <a href="/mypage">마이페이지</a>
+          <a href="/" className='menu'>홈</a>
+          <a href="/mypage" className='menu'>마이페이지</a>
         </nav>
+        <InputBase
+          placeholder="극 제목을 검색해보세요."
+          inputProps={{ 'aria-label': 'search' }}
+        />
+        <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
+          <SearchIcon />
+        </IconButton>
+        <Button variant="contained" color="primary">
+          로그인
+        </Button>
     </header>
   );
 }
