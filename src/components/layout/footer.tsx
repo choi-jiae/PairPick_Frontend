6f7ce8af5b8275f12f5145031dbce5afa6e7e580
choi-jiae <chlwldo2002@gmail.com>
2025-02-07 23:22:01 +0900

    relative style & footer

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import { BottomNavigation, BottomNavigationAction } from '@mui/material';
+import '../../css/footer.css';
 
 const Footer = () => {
     return (
-        <footer>
-            <h2>footer</h2>
+        <footer className="bottom-nav">
+            <BottomNavigation
+                showLabels
+                sx={{
+                    width: '100%',
+                    position: 'fixed',
+                    bottom: 0,
+                    backgroundColor: '#FDFDFD',
+                }}    
+            >
+                <BottomNavigationAction label="Home" value="home" />
+                <BottomNavigationAction label="Search" value="search" />
+                <BottomNavigationAction label="MyPage" value="mypage" />
+            </BottomNavigation>
         </footer>
     )
 }
--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import Header from './header';
+import Footer from './footer';
 
 const Layout = (props: {
     children: React.ReactNode;
 }) => {
     return (
         <div>
-            <header>
-                <Header />
-            </header>
+            <Header />
             <main>
                 {props.children}
             </main>
+            <Footer />
         </div>
     )
 }
